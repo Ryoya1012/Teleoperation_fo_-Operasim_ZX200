b7b5fb55a81462caeebbc394a11a595cc890c662
Ryoya SATO <satoryoya1012711@gmail.com>
2026-06-17 16:09:27 +0900

    Add a file

--- my_teleop/my_teleop/zx200_ps5_teleop.py
+++ my_teleop/my_teleop/zx200_ps5_teleop.py
@@ -1,7 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Joy
-from std_msgs.msg import Float64
+from com3_msgs.msg import JointCmd
 
 class ZX200PS5Teleop(Node):
     def __init__(self):
@@ -9,10 +9,7 @@ class ZX200PS5Teleop(Node):
 
         self.subscription = self.create_subscription( Joy, 'joy', self.joy_callback, 10)
 
-        self.pub_swing = self.create_publisher( Float64, '/zx200/swing/cmd', 10)
-        self.pub_boom = self.create_publisher( Float64, '/zx200/boom/cmd', 10)
-        self.pub_arm = self.create_publisher( Float64, '/zx200/arm/cmd', 10)
-        self.pub_bucket = self.create_publisher( Float64, '/zx200/bucket/cmd', 10)
+        self.pub_front = self.create_publisher( JointCmd, '/zx200/front_cmd', 10)
         
         self.target_swing = 0.0
         self.target_boom = 0.0
@@ -31,27 +28,28 @@ class ZX200PS5Teleop(Node):
     def timer_callback( self):
         # 1. 上部旋回(左ステック左右: axes[0])
         self.target_swing += self.current_axes[0] * self.speed_scale
-        cmd_swing = Float64()
-        cmd_swing.data = float(self.target_swing)
-        self.pub_swing.publish(cmd_swing)
 
         # 2. アーム(左ステック上下: axes[1])
         self.target_arm -= self.current_axes[1] * self.speed_scale
-        cmd_arm = Float64()
-        cmd_arm.data = float(self.target_arm)
-        self.pub_arm.publish(cmd_arm)
 
         # 3. バケット(右ステック左右: axes[3])
         self.target_bucket += self.current_axes[3] * self.speed_scale
-        cmd_bucket = Float64()
-        cmd_bucket.data = float( self.target_bucket)
-        self.pub_bucket.publish(cmd_bucket)
-
+       
         # 4. ブーム(右ステック上下: axes[4])
         self.target_boom += self.current_axes[4] * self.speed_scale
-        cmd_boom = Float64()
-        cmd_boom.data = float( self.target_boom)
-        self.pub_boom.publish(cmd_boom)
+
+        msg = JointCmd()
+
+        msg.joint_name = [ 'swing_joint', 'boom_joint', 'arm_joint', 'bucket_joint']
+
+        msg.position = [
+                float( self.target_swing),
+                float( self.target_boom),
+                float( self.target_arm),
+                float( self.target_bucket)
+                ]
+
+        self.pub_front.publish( msg)
 
 def main(args=None):
     rclpy.init(args=args)
@@ -66,3 +64,4 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
+
